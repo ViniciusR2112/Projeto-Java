6c86dc8b5cc711f5ad72428af674abb51bcd294c
ViniciusR2112 <97138276+ViniciusR2112@users.noreply.github.com>
2024-10-23 19:44:17 -0300

    Alguns comandos inicias da aula de Java
    Alguns comandos de soma e raiz quadrada em java.

---
+++ project.java
@@ -0,0 +1,203 @@
+
+
+import java.util;
+import java.Scanner;
+
+
+
+Scanner sc = new Scanner(String args[] );
+
+int a = nextint();
+int b = nextint(); soma;
+
+
+
+if( soma ! = 0){
+
+  
+
+} else if( soma ==0){
+
+for( int 0; int > 2; i++ ){
+
+System.out.print("a+b" );
+System.out.print("Captura completa" );
+
+
+}
+
+}
+
+
+double x
+
+double y
+
+x = Math.sqrt(x);
+y = Math.sqrt(y);
+
+System.out.print("x,y");
+System.out.print("x,y");
+System.out.print("raiz quadrada" +  )
+
+
+public class re (){
+
+    public  double area;
+    public  double  medida;
+
+
+     public void area(){
+        return width * height;
+
+
+
+    }
+    
+        public void area(){
+        return width * height;
+
+        
+
+    }
+  
+      
+    public void geometrica(){
+        return 2 * (width * height);
+
+        
+
+    }
+  
+         
+     public void diagonal(){
+     retunr Math.sqrt(height * width * height);
+
+        
+
+    }
+  
+
+
+
+
+
+}
+
+
+
+// Exercício 2  
+
+public class func(){
+
+
+    String nome;
+    Tax: doulble 
+    gross double
+    
+        public double sal(){
+            return sal - tax;
+
+
+
+        }
+
+    public void  double sal( double porcent ){
+    
+    return  * porcent / 100
+    }
+
+
+    // exercício 3 
+
+    public class alumo(){
+
+        public static string name;
+        public static double grade1;
+        public static double grade2;
+
+       
+
+public double grade(){
+retunr grade1 + grade2 +grade3; 
+
+}
+
+        public double nota () 
+        if( nota !-= 60){
+            retunr 
+            System.out.print("Reprovado");
+
+        }
+
+        Scanner sc = new Scanner(System.in);
+
+        Student.name = sc.nextInt();
+        Student.grade1 = sc.nextInt();
+        Student.grade2 = sc.nextInt();
+        Student.grade3 = sc.nextInt();
+    System.out.print("grade")
+        if(Student.grade < 60 ){
+
+            System.out.println("FAILED");
+
+        }else{
+
+            System.out.println("PASS");
+
+        }
+
+        sc.close();
+    }
+
+    String.format();
+    // construtor
+
+    public prod(String name, double valor){
+
+        this.name = name;
+        this.price = price;
+        
+        }
+
+        /// sobrecarga
+
+        produto p = new produto( double dado, int saldo  ){]
+        // difereça é a lista de parâmretrôs( sdsd, ssss)
+        
+        
+        }
+
+
+        import java.util.io;
+        import java.util.Scanner;
+
+        Int a = NextInt();
+        Int b = NextInt();
+        
+
+        // login
+ Scanner s = new Scanner();
+         a = a+b;
+
+        if( a ==123 && b == Rush*2112   ){
+
+            System.out.print("Logado")
+
+           } else if( a != 23 && b != Rush*2112 ){
+
+            System.out.print("Erro ao logar")
+
+
+           }
+        
+        public class cliente {
+
+            String nome;
+            int idade;
+             int Valor;
+            
+
+        }
+
+        s.close();
